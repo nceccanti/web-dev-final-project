fbd5269db473d84c9c7c32e082d546db6299c4f4
byron <byron@calisium.com>
2021-11-07 19:29:24 -0700

    Logo and styling update

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -1,11 +1,20 @@
 import React, {Component} from 'react';
+import backgroundImg from '../img/home-background-medium.jpg';
+import '../index.css';
 
 export default class Home extends Component {
   
     render() {
         return (
             <div>
-                <p>home</p>
+                <img src={backgroundImg} id="bg" alt=""></img>
+                <h3>Plants are great! But taking care of them can be hard.</h3><br/>
+                <h3>That's why we made HydroClock!</h3>
+                <br/><br/>
+                <ul>
+                    <li>Get watering reminders</li>
+                    <li>Find plant care tips based on plant type</li>
+                </ul>
             </div>        
         )
     }
