c6ebeb2b6fff1f12863cc8c9af339e847435460b
byron <byron@calisium.com>
2021-11-07 19:30:53 -0700

    Logo and styling update

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { ReactComponent as LogoSvg } from '../img/logo.svg';
+import '../index.css';
 
 export default class Navbar extends Component {
 
   render() {
     return (
       <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-        <Link to="/" className="navbar-brand">HydroPlant</Link>
+        <Link to="/home" className="logo"><LogoSvg /></Link>
         <div className="collapse navbar-collapse">
         <ul className="navbar-nav mr-auto">
           <li className="navbar-item">
-          <Link to="/dashboard" className="nav-link">dashboard</Link>
+          <Link to="/dashboard" className="nav-link">Dashboard</Link>
           </li>
           <li className="navbar-item">
           <Link to="/addplant" className="nav-link">Add Plant</Link>
